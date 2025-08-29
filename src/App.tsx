0d48832c39786ed13c685208a9a1334c9a7f10cc
Maël Pendilhas <mpendilhas@gmail.com>
2025-08-29 22:44:42 +0200

    continu to learn react

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,27 @@
 import React from 'react';
 import './App.css';
 
-function Button({title}: {title: string}) {
+interface test {
+	title: string;
+	disabled?: boolean;
+	onClick?: () => void;
+}
+
+function Button({title, disabled, onClick}: test) {
 	return (
-		<button>{title}</button>
+		<button disabled={disabled} onClick={onClick}>{title}</button>
 	);
 }
 
 export default function App() {
+	let disable: boolean = false;
+
 	return (
 		<>
 			<title>inscription page</title>
-			<Button title="test" />
+			<Button title="disable button" onClick={() => disable = false}/>
+			<Button title="enable button" onClick={() => disable = true}/>
+			<Button title="je suis le bouton que je pense etre" disabled={disable}/>
 		</>
 	);
 }
