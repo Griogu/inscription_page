d8d9b75cd54aa2f13c8d32391895e2994f658b9e
Griogu <mpendilhas@gmail.com>
2025-08-30 18:52:59 +0200

    start the inscription page: do the firstname, lastname, email and password input

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,87 @@
 import React, { useState } from 'react';
 import './App.css';
 
-interface test {
-	title: string;
-	disabled?: boolean;
-	onClick?: () => void;
+function FirstNameInput({ value, onChange }:
+	{ value: string; onChange: (e: React.ChangeEvent<HTMLInputElement>) => void }) {
+  return (
+    <label>
+      First Name:
+      <input className="firstname"
+	  			placeholder="John"
+	  			type="text"
+				value={value}
+				onChange={onChange}
+				/>
+    </label>
+  );
 }
 
-function Button({title, disabled, onClick}: test) {
+function LastNameInput({ value, onChange }:
+	{ value: string; onChange: (e: React.ChangeEvent<HTMLInputElement>) => void }) {
+  return (
+    <label>
+      Last Name:
+      <input className="lastname"
+	  			placeholder="Doe"
+	  			type="text"
+				value={value}
+				onChange={onChange}
+				/>
+    </label>
+  );
+}
+
+function EmailInput({ value, onChange }:
+	{ value: string; onChange: (e: React.ChangeEvent<HTMLInputElement>) => void }) {
 	return (
-		<button disabled={disabled} onClick={onClick}>{title}</button>
+		<label>
+		  Email:
+		  <input className="email"
+		  			placeholder="JohnDoe@gmail.com"
+		  			type="email"
+					value={value}
+					onChange={onChange}/>
+		</label>
+	);
+}
+
+function PasswordInput({ value, onChange }:
+					   { value: string, onChange: (e: React.ChangeEvent<HTMLInputElement>) => void }) {
+	return (
+		<label>
+		  Password:
+		  <input className="password"
+		  			placeholder="Password"
+		  			type="password"
+					value={value}
+					onChange={onChange}/>
+		</label>
 	);
 }
 
 export default function App() {
-  const [disable, setDisable] = useState(false);
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   return (
-    <>
-      <title>inscription page</title>
-      <Button title="disable button" onClick={() => setDisable(true)} />
-      <Button title="enable button" onClick={() => setDisable(false)} />
-      <Button title="je suis le bouton que je pense être" disabled={disable} />
-    </>
+    <div className="form-container">
+	  <title>Inscription page</title>
+	  <h1 className="header">Inscription page</h1>
+      <FirstNameInput value={firstName}
+	  					onChange={(e) => setFirstName(e.target.value)}
+						/>
+      <LastNameInput value={lastName}
+	  					onChange={(e) => setLastName(e.target.value)}
+						/>
+	  <EmailInput value={email}
+					onChange={(e) => setEmail(e.target.value)}
+					/>
+	  <PasswordInput value={password}
+	  					onChange={(e) => setPassword(e.target.value)}
+						/>
+    </div>
   );
 }
+
